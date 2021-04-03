1d4b03de206e7934f1f4d3174fd013e03f992411
Ryan Kirsch <agalloch88@gmail.com>
2021-04-03 09:36:14 -0400

    day one buildout of architecture, room and channel components, buttons, etc

---
+++ src/data/SidebarData.js
@@ -0,0 +1,33 @@
+import CommentIcon from '@material-ui/icons/Comment';
+import ForumIcon from '@material-ui/icons/Forum';
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleIcon from '@material-ui/icons/People';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+export const sidebarItemsData = [
+    {
+        icon: <CommentIcon />,
+        text: "Threads"
+    },
+    {
+        icon: <ForumIcon />,
+        text: "All DMs"
+    },
+    {
+        icon: <AlternateEmailIcon />,
+        text: "Mentions & Reactions"
+    },
+    {
+        icon: <BookmarkBorderIcon />,
+        text: "Saved Items"
+    },
+    {
+        icon: <PeopleIcon />,
+        text: "People & Groups"
+    },
+    {
+        icon: <MoreVertIcon />,
+        text: "More"
+    }
+]
